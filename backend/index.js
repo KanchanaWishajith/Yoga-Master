133d06bf982ab574ba66766930aa6acd948a56d8
Kanchana Samarasinghe <kanchana4592@gmail.com>
2024-11-21 20:52:29 +0530

    Connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = 3000
+const port = process.env.PORT || 5000;
 console.log("Db user name", process.env.DB_USER)
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+app.use(express.json())
 
 //mongodb connection
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://wkwsamarasingha:gwDyBPUgdZo4jT2o@yoga-master.v1kbbtv.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-master.v1kbbtv.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,6 +27,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("yoga-master");
+    const userCollections = database.collection("usrs");
+    const classCollections = database.collection("classes");
+    const cartCollections = database.collection("cart");
+    const paymentCollections = database.collection("payments");
+    const enrolledCollection = database.collection("enrolled");
+    const appliedCollection = database.collection("applied");
+
+    //classes routes here
+    app.post('/new-class', async(req, res) => {
+      const newClass = req.body;
+      //newClass.availableSeats = parseInt(newClass.availableSeats);
+      const result = await classCollections.insertOne(newClass);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -34,7 +56,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 2024')
+  res.send('Hello World! 2024 Kanchana')
 })
 
 app.listen(port, () => {
