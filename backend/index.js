8c8f23cc20eeba32f435b6ddfee1c1a3cba25d39
Kanchana Samarasinghe <kanchana4592@gmail.com>
2025-03-07 22:27:42 +0530

    get single class details

--- backend/index.js
+++ backend/index.js
@@ -104,6 +104,14 @@ app.get('/approved-classes', async(req, res) =>{
   res.send(result);
 });
 
+//get single class details
+app.get('/class/:id', async (req, res) =>{
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await classCollections.findOne(query);
+  res.send(result);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World! 2024 Kanchana');
 });
